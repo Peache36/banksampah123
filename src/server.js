53939b3c11ab2b8157a2e29e24621f871e5bae35
ansxy <ansarfadillah17@gmail.com>
2023-07-02 02:04:55 +0800

    refactoring router

--- src/server.js
+++ src/server.js
@@ -1,18 +1,20 @@
 const express = require('express');
 // const router = require('./routes/index.js');
 const app = express();
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+const userRouter = require('./routes/user.routes');
+const adminRouter = require('./routes/admin.routes');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 			
 // parse application/json
 app.use(bodyParser.json())
+app.use('/admin',adminRouter)
+app.use('/user', userRouter)
 
-// app.use('/flights', router)
-
-app.listen(3000, () =>
-    console.log('Example app listening on port 3000!'),
+app.listen(3001, () =>
+    console.log('Example app listening on port 3001!'),
 );
 
 module.exports = app;
